25514a3a4368324086a9119b9c91b03e52eaec3b
Sampath663 <sampatht248@gmail.com>
2021-04-24 21:27:35 +0530

    implement read and insert methods

--- Product/src/com/ProductService.java
+++ Product/src/com/ProductService.java
@@ -17,4 +17,27 @@ public class ProductService {
 
 	Product productObj = new Product();
 	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	
+	public String readProducts()
+	 {
+		return productObj.readProducts(); 
+	 }
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertProduct
+	(@FormParam("productCode") String productCode,
+	 @FormParam("productName") String productName,
+	 @FormParam("productPrice") String productPrice,
+	 @FormParam("productDesc") String productDesc)
+	{
+	 String output = productObj.insertProduct(productCode, productName, productPrice, productDesc);
+	return output;
+	}
+	
 }
--- Product/src/model/Product.java
+++ Product/src/model/Product.java
@@ -23,6 +23,82 @@ public class Product {
 		return con;	
 	}
 	
+	public String insertProduct(String code, String name, String price, String desc)
+	{
+		String output = "";
+	 
+	try
+	{
+		 Connection con = connect();
+		 
+		 if(con == null)
+		 {return "Error while connecting to the database for inserting."; }
+		 
+		 //create a prepared statement
+		 String query = " insert into products(`productID`,`productCode`,`productName`,`productPrice`,`productDesc`)"
+				 + " values (?, ?, ?, ?, ?)";
+		 PreparedStatement preparedStmt = con.prepareStatement(query);
+		 // binding values
+		 preparedStmt.setInt(1, 0);
+		 preparedStmt.setString(2, code);
+		 preparedStmt.setString(3, name);
+		 preparedStmt.setDouble(4, Double.parseDouble(price));
+		 preparedStmt.setString(5, desc);
+		// execute the statement
+		 preparedStmt.execute();
+		 con.close();
+		 output = "Inserted successfully";
+		 		 
+	}
+	catch(Exception e)
+	{
+		 output = "Error while inserting the item.";
+		 System.err.println(e.getMessage());
+	}
+	return output;
 	
+	 }
+	public String readProducts()
+	 {
+		String output = "";
+	 try
+	 {
+		 Connection con = connect();
+		 if (con == null)
+		 {return "Error while connecting to the database for reading."; }
+		 // Prepare the html table to be displayed
+		 output = "<table border='1'><tr><th>Product Code</th><th>Product Name</th>" +
+				 "<th>Product Price</th>" +"<th>Product Description</th>" + "<th>Update</th><th>Remove</th></tr>";
+
+		 String query = "select * from products";
+		 Statement stmt = con.createStatement();
+		 ResultSet rs = stmt.executeQuery(query);
+		 // iterate through the rows in the result set
+		 while (rs.next())
+		 	{
+			 String productID = Integer.toString(rs.getInt("productID"));
+			 String productCode = rs.getString("productCode");
+			 String productName = rs.getString("productName");
+			 String productPrice = Double.toString(rs.getDouble("productPrice"));
+			 String productDesc = rs.getString("productDesc");
+			 // Add into the html table
+			 output += "<tr><td>" + productCode + "</td>";
+			 output += "<td>" + productName + "</td>";
+			 output += "<td>" + productPrice + "</td>";
+			 output += "<td>" + productDesc + "</td>";
+			 // buttons
+			 output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"+ "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"+ "<input name='productID' type='hidden' value='" + productID+ "'>" + "</form></td></tr>";
+		 	}
+		 	con.close();
+		 	// Complete the html table
+		 	output += "</table>";
+	 		}
+	 catch (Exception e)
+	 {
+		 output = "Error while reading the items.";
+		 System.err.println(e.getMessage());
+	 }
+	 	return output;
+	 }
 
 }
